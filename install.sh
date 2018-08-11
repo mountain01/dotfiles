ee47b91f9b05c2de71b5e1460b10dafc46bd3e16
Matt Edwards <edwardsjm01@gmail.com>
2018-08-11 02:22:36 -0600

    add .gitignore_global

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -sfv "$DOTFILES_DIR/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
