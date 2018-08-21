a81ea5a0bb784347fe806f2cb2a5f5f5b316fb39
Matt Edwards <edwardsjm01@gmail.com>
2018-08-20 22:54:54 -0600

    add new spaceship theme

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sfv "$DOTFILES_DIR/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
+ln -sfv "$DOTFILES_DIR/.spaceship-custom" ~
